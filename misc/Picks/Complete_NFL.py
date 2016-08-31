a199f9adea42e0d5a12acce8b23542e9337bc843
Seth Lawler <slawler@gmu.edu>
2016-08-31 14:42:15 -0400

    Add files via upload

--- misc/Picks/Complete_NFL.py
+++ misc/Picks/Complete_NFL.py
@@ -12,8 +12,9 @@ Created on Tue Aug 30 09:33:12 2016
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-
+import fileinput
 #------------------------------BEGIN SCRIPT----------------------------------#
+week            = str(1)
 upcoming_games  = "http://www.si.com/nfl/scoreboard?week=1%2C2"
 concluded_games = "http://www.si.com/nfl/scoreboard?week=0%2C4"
 picks           = r"C:\Users\slawler\Desktop\picks.txt"
@@ -33,6 +34,7 @@ tables = soup.find_all("a", class_="unskinned")
 matchups = []
 print('\nThis Weeks Match-Ups\n')
 
+# Scrape SI html for Upcoming Match data
 for table in tables:
     try:
         team = table.contents[1].decode_contents()
@@ -42,15 +44,36 @@ for table in tables:
             continue
     except:
         continue
+    
+# Write to matchups to file, print to screen     
+with open('Week{0}_Matchups.txt'.format(week),'w') as f:
+    f.write('team1\tteam2\n')    
+    for i, matchup in enumerate(matchups):
+        if i == 2:
+            game = i/2+1
+            print game, matchups[i], matchups[i+1]
+            f.write(matchups[i]+'\t'+ matchups[i+1]+'\n')
+        elif i%2 == 0:
+            game =i/2+1
+            print game, matchups[i], matchups[i+1]
+            f.write(matchups[i] +'\t'+ matchups[i+1] +'\n')
 
-for i, matchup in enumerate(matchups):
-    if i == 2:
-        game = i/2+1
-        print game, matchups[i], matchups[i+1]
-    elif i%2 == 0:
-        game =i/2+1
-        print game, matchups[i], matchups[i+1]
 
+# Creat javascript doc for weeks games
+df = pd.read_csv('Week{0}_Matchups.txt'.format(week),sep='\t')
+f = fileinput.input('jsquiz.js')
+i=1
+with open('jsquiz{0}.js'.format(week),'w') as fout:
+    for line in f:
+        if "team1" in line and "team2" in line:
+            t1,t2 = df.team1.iloc[i-1],df.team2.iloc[i-1]
+            newline= line.replace("team1",t1).replace("team2",t2)
+            print i, newline
+            i+=1
+            fout.write(newline)
+        else:
+            fout.write(line)
+       
 #===============PART 2: Concluded Games--Get This Weeks Results==============#
 
 r = requests.get(concluded_games)
@@ -109,7 +132,7 @@ for i, table in enumerate(tables):
     winners.append(winner)
 
 #===================PART 3: Check Individual Results & Tally Score===========#
-
+'''
 df = pd.read_csv(picks, sep = '\t')
 
 score_tally = {}
@@ -123,3 +146,4 @@ for picker in df:
    
 for picker in sorted(score_tally, key=score_tally.get, reverse=True):
   print picker, score_tally[picker]
+''' 
