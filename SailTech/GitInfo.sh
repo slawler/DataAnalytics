57f9a3ae5febd213cea325b5290e882c49f42ab0
Seth Lawler <slawler@gmu.edu>
2016-11-06 23:41:01 -0500

    Add files via upload

---
+++ SailTech/GitInfo.sh
@@ -0,0 +1,9 @@
+# First time setup
+git clone https://github.com/slawler/DataAnalytics localGit
+
+# Update files
+git add Temperature.log
+git commit -m 'Added Entries to log'
+git push origin master
+
+
