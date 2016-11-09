04c9c0fb96a216be60dce527e8d4038c00bfeead
Seth <slawler@gmu.edu>
2016-11-08 20:06:03 -0500

    Reomved Sailtech

--- SailTech/GitInfo.sh
+++
@@ -1,9 +0,0 @@
-# First time setup
-git clone https://github.com/slawler/DataAnalytics localGit
-
-# Update files
-git add Temperature.log
-git commit -m 'Added Entries to log'
-git push origin master
-
-
